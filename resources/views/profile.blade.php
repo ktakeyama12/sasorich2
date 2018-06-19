7abaaf120e780bf09ac566ca375028fb9562149c
kazuki.takeyama <kazuki.takeyama@rakuten.com>
2018-06-19 06:58:32 +0000

    join v2

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,13 +3,15 @@
 @section('content')
     <h1>My Page</h1>
          Name: {{ Auth::user()->name }}
+         <br>
+         Email: {{ Auth::user()->email }}
         <br>
-        Events that you're going to join
-        <br>
+        Events that you're going to join:
         <br>
         <?php
         foreach($name as $name){
-            print $name . " ";
+            print $name;
+            ?><br><?php
         }
         ?>
 @endsection
